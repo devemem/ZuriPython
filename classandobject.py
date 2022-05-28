5961cc295a72a02c71ab38d7e11e616fc567b4f8
devemem <udohememisong@gmail.com>
2022-05-28 13:41:55 +0100

    classAndobject task

--- classandobject.py
+++ classandobject.py
@@ -1,17 +1,35 @@
 class Student:
-    
     # [assignment] Skeleton class. Add your code here
-    def __init__(self, Name, Age, Tracks, Score ):
-        pass
-        n
+    def __init__(self,name,age,tracks,score):
+        self.name = name
+        self.age = int(age)
+        self.tracks = [tracks]
+        self.score = float(score)
 
+    def change_name(self, new_name):
+        self.name=new_name
+        return self.name
+        #return new_name
+
+    def change_age(self,new_age):
+        self.age= new_age
+        return self.age
+        #return new_age
+
+    def add_track(self,new_track):
+        self.tracks = (new_track) 
+        return self.tracks
+        #return new_track
+
+    def get_score(self):
+        return self.score
 
 
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+print(Bob.change_name("Peter"))
+print(Bob.change_age(34))
+print(Bob.add_track("UI/UX"))
+print(Bob.get_score())
